b8b48b9eee7b362770e0f904743640b007412a4f
Istiakahmed180 <Istiakahmed180@gmail.com>
2023-03-04 22:57:48 +0600

    backend data code add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const categories = require("./Data/Categories.json");
+const news = require("./Data/News.json");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -12,6 +13,26 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/all_news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
